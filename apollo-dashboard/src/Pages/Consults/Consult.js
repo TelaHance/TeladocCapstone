ec4fc452de8b5a426a7455c01cd5391ae55f446d
Alan Roddick <aroddick16@gmail.com>
2020-12-06 17:45:45 -0800

    Cleaned up code

--- apollo-dashboard/src/Pages/Consults/Consult.js
+++ apollo-dashboard/src/Pages/Consults/Consult.js
@@ -1,43 +1,27 @@
 import React from "react";
-import { Jumbotron } from "react-bootstrap";
 import {fetchWithToken} from "../../Util/fetch";
 import useSWR from "swr";
-import ReactJson from "react-json-view";
-import {Container, Col, Row} from 'react-bootstrap';
+import {Col, Row} from 'react-bootstrap';
 import TranscriptEditor from "@bbc/react-transcript-editor";
-import { TimedTextEditor } from "@bbc/react-transcript-editor/TimedTextEditor";
 import DEMO_TRANSCRIPT from "./assets/transcript.json";
 import DEMO_MEDIA from "./assets/media.m4a";
 
 
 
 const Consult = ({match}) =>{
-    console.log(match)
     const { params: {consultId} } = match;
     const awsToken = process.env.REACT_APP_CONSULT_API_KEY;
     const { data: response, error, mutate: mutateConsults } = useSWR(
         [`https://53q2e7vhgl.execute-api.us-west-2.amazonaws.com/dev/consult-get-by-id?consult_id=${consultId}`, awsToken],
         fetchWithToken
     );
-    console.log(response)
-    console.log(error)
     let consult = null;
     if (response) {
         consult = JSON.parse(response.body);
     }
-    console.log(consult);
-    const transcript = require('./assets/transcript.json');
-    // const DEMO_MEDIA = require('./assets/audio.mp3');
     var binaryData = [];
     binaryData.push(DEMO_MEDIA);
-    // const fileURL = window.URL.createObjectURL(new Blob(binaryData, {type: "audio/mp3"}))
-    // const fileURL = URL.createObjectURL(DEMO_MEDIA);
     return (
-        // <Container className="mb-5">
-        //     <Row className="text-left">
-        //         <ReactJson src={consult} />
-        //     </Row>
-        // </Container>
         <Row>
             <Col md={9}>
                 <TranscriptEditor
