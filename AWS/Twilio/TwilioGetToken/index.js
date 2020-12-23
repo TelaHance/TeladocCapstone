02f914d0cabc7af52f3c1a310a1ae9f6576a206b
Tanay Komarlu <54658599+tkomarlu@users.noreply.github.com>
2020-12-22 22:58:46 -0800

    Update index.js

--- AWS/Twilio/TwilioGetToken/index.js
+++ AWS/Twilio/TwilioGetToken/index.js
@@ -7,7 +7,7 @@ const client = new twilio.RestClient(accountSid, authToken);
 
 exports.handler = (event, context, callback) => {
 	var capability = new twilio.Capability(accountSid, authToken);
-	capability.allowClientOutgoing("AP355c3961b103c04674dc652d97514ebf");
+	capability.allowClientOutgoing("FILL-IT-IN");
 
     callback(null, JSON.stringify({token: capability.generate()}));
 };
