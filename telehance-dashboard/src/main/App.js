70354ade34832686d9a9f5e32210e638ca894577
GrantClark209 <grantclark1999@gmail.com>
2020-12-29 12:22:29 -0800

    Small changes to display

--- telehance-dashboard/src/main/App.js
+++ telehance-dashboard/src/main/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import "./App.css";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Container } from "react-bootstrap";
 import { Route, Switch } from "react-router-dom";
-import AppFooter from "./Components/Footer/AppFooter";
 import Home from "./Pages/Home/Home";
 import Profile from "./Pages/Profile/Profile";
 import ConsultDashboard from "./Pages/Consults/ConsultDashboard";
@@ -25,7 +23,7 @@ function App() {
     return (
         <div className="App">
             <AppNavbar />
-            <Container className="flex-grow-1 mt-5">
+            <div className="flex-grow-1 mt-5">
                 <Switch>
                     <Route path="/" exact component={Home} />
                     <AuthorizedRoute exact path="/consults" component={ConsultDashboard} authorizedRoles={["admin", "doctor", "patient"]}  />
@@ -35,8 +33,7 @@ function App() {
                     <PrivateRoute path="/consults/:consultId" component={(props) => <Consult {...props}/>} />
                     <AuthorizedRoute path="/patients" component={Patient} authorizedRoles={["doctor"]}/>
                 </Switch>
-            </Container>
-            <AppFooter />
+            </div>
         </div>
     );
 }
