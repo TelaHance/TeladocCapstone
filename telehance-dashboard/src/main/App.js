0826aa3f4c98db86f367174366077c26dd320705
Alan Roddick <aroddick16@gmail.com>
2021-01-20 10:03:49 -0800

    Removing unnecessary code and fixing warnings

--- telehance-dashboard/src/main/App.js
+++ telehance-dashboard/src/main/App.js
@@ -3,6 +3,17 @@ import "./App.css";
 import { useAuth0 } from "@auth0/auth0-react";
 import AppLayout from "./Components/Layout/AppLayout";
 import Loading from "./Components/Loading/Loading";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Profile from "./Pages/Profile/Profile";
+import ConsultDashboard from "./Pages/Consults/ConsultDashboard/ConsultDashboard";
+import Consult from "./Pages/Consults/Consult";
+import Admin from "./Pages/Admin/Admin"
+import AuthorizedRoute from "./Components/Auth/AuthorizedRoute";
+import TwilioCall from "./Pages/Patients/TwilioCall";
+import Patient from "./Pages/Patients/Patients";
+import PrivateRoute from "./Components/Auth/PrivateRoute";
+import Navbar from "./Components/Nav/Navbar";
 
 function App() {
     const {isLoading} = useAuth0();
@@ -11,9 +22,18 @@ function App() {
     }
 
     return (
-        <div>
-            <AppLayout />
-        </div>
+        <AppLayout>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <AuthorizedRoute exact path="/consults" component={ConsultDashboard} authorizedRoles={["admin", "doctor", "patient"]}  />
+                <PrivateRoute path="/profile" component={Profile} />
+                <AuthorizedRoute path="/admin" component={Admin} authorizedRoles={["admin"]}  />
+                <AuthorizedRoute path="/TwilioCall/:patientId&:phoneNumber" component={TwilioCall} authorizedRoles={["doctor"]}/>
+                <PrivateRoute path="/consults/:consultId" component={(props) => <Consult {...props}/>} />
+                <AuthorizedRoute path="/patients" component={Patient} authorizedRoles={["doctor"]}/>
+            </Switch>
+        </AppLayout>
     );
 }
 
--- telehance-dashboard/src/main/Components/Layout/AppLayout.js
+++ telehance-dashboard/src/main/Components/Layout/AppLayout.js
@@ -1,35 +1,20 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import "./AppLayout.module.css";
-import { Container } from "react-bootstrap";
-import { Route, Switch } from "react-router-dom";
-import AppFooter from "../Footer/AppFooter";
-import Home from "../../Pages/Home/Home";
-import Profile from "../../Pages/Profile/Profile";
-import ConsultDashboard from "../../Pages/Consults/ConsultDashboard/ConsultDashboard";
-import Consult from "../../Pages/Consults/Consult";
-import Admin from "../../Pages/Admin/Admin"
-import AuthorizedRoute from "../Nav/AuthorizedRoute";
-import TwilioCall from "../../Pages/Patients/TwilioCall";
-import Patient from "../../Pages/Patients/Patients";
-import PrivateRoute from "../Auth/PrivateRoute";
-import AppNavbar from "../Nav/AppNavbar";
-import Navbar from "../Nav/Navbar";
 
-function AppLayout() {
+function AppLayout({ children }) {
     return (
         <div>
-            <Navbar />
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <AuthorizedRoute exact path="/consults" component={ConsultDashboard} authorizedRoles={["admin", "doctor", "patient"]}  />
-                <PrivateRoute path="/profile" component={Profile} />
-                <AuthorizedRoute path="/admin" component={Admin} authorizedRoles={["admin"]}  />
-                <AuthorizedRoute path="/TwilioCall/:patientId&:phoneNumber" component={TwilioCall} authorizedRoles={["doctor"]}/>
-                <PrivateRoute path="/consults/:consultId" component={(props) => <Consult {...props}/>} />
-                <AuthorizedRoute path="/patients" component={Patient} authorizedRoles={["doctor"]}/>
-            </Switch>
+            {children}
         </div>
-    )
+    );
+}
+
+AppLayout.propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.element),
+      PropTypes.element.isRequired
+    ])
 }
 
 export default AppLayout;
