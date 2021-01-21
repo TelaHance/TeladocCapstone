451cf3343e5ab6d826a284fd76435c34b84062e0
Alan Roddick <aroddick16@gmail.com>
2021-01-20 21:47:04 -0800

    Added breadcrumb bar

--- telehance-dashboard/src/main/Pages/Profile/Profile.js
+++ telehance-dashboard/src/main/Pages/Profile/Profile.js
@@ -4,37 +4,19 @@ import { Row, Container, Col, Badge } from "react-bootstrap";
 import ReactJson from "react-json-view";
 import useSWR from "swr";
 import {fetchWithUser} from "../../Util/fetch";
+import BreadcrumbBar from "../../Components/BreadcrumbBar/BreadcrumbBar";
 const Profile = () => {
-    const awsToken = process.env.REACT_APP_MANAGEMENT_API_KEY;
-    const { user } = useAuth0();
-    const { name, picture, email, sub} = user;
-    const { data: roleInfo} = useSWR(
-        ["https://qf5ajjc2x6.execute-api.us-west-2.amazonaws.com/dev/user-by-id", awsToken, 'POST', sub.split('|')[1]],
-        fetchWithUser);
-    return (
-        <Container className="mb-5">
-            <Row className="align-items-center profile-header mb-5 text-center text-md-left">
-                <Col md={2}>
-                    <img
-                        src={picture}
-                        alt="Profile"
-                        className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
-                    />
-                </Col>
-                <Col md>
-                    <h2>{name}</h2>
-                    <p className="lead text-muted">{email}</p>
-                    { roleInfo ?
-                        <Badge variant="info">{JSON.parse(roleInfo.body).role}</Badge>:
-                        <span>Loading role...</span>
-                    }
-                </Col>
-            </Row>
-            <Row className="text-left">
-                <ReactJson src={user} />
-            </Row>
-        </Container>
-    );
+  const awsToken = process.env.REACT_APP_MANAGEMENT_API_KEY;
+  const { user } = useAuth0();
+  const { name, picture, email, sub} = user;
+  const { data: roleInfo} = useSWR(
+    ["https://qf5ajjc2x6.execute-api.us-west-2.amazonaws.com/dev/user-by-id", awsToken, 'POST', sub.split('|')[1]],
+    fetchWithUser);
+  return (
+    <div>
+        <BreadcrumbBar page="Profile" />
+    </div>
+  );
 };
 
 export default Profile;
