8b86fd28d5feffe4488332507be7ba07e135b849
Alan Roddick <aroddick16@gmail.com>
2021-01-23 12:17:15 -0800

    First draft of Profile page and clean up css

--- telehance-dashboard/src/main/Components/BreadcrumbBar/BreadcrumbBar.js
+++ telehance-dashboard/src/main/Components/BreadcrumbBar/BreadcrumbBar.js
@@ -1,24 +1,24 @@
 import React from "react";
-import "./BreadcrumbBar.css";
+import styles from "./BreadcrumbBar.module.css";
 
 
 const BreadcrumbBar = (props) => {
   return (
-    <div className="breadcrumb-bar">
-      <div className="container-fluid">
+    <div className={styles["breadcrumb-bar"]}>
+      <div className={styles["container-fluid"]}>
         <div className="row align-items-center">
           <div className="col-md-12 col-12">
-            <nav aria-label="breadcrumb" className="page-breadcrumb">
+            <nav aria-label="breadcrumb" className={styles["page-breadcrumb"]}>
               <ol className="breadcrumb">
-                <li className="breadcrumb-item">
+                <li className={styles["breadcrumb-item"]}>
                   <a href="/">Home</a>
                 </li>
-                <li className="breadcrumb-item active" aria-current="page">
+                <li className={styles["breadcrumb-item"] + " " +  styles["active"]} aria-current="page">
                   {props.page}
                 </li>
               </ol>
             </nav>
-            <h2 className="breadcrumb-title">{props.page}</h2>
+            <h2 className={styles["breadcrumb-title"]}>{props.page}</h2>
           </div>
         </div>
       </div>
--- telehance-dashboard/src/main/Pages/Profile/Profile.js
+++ telehance-dashboard/src/main/Pages/Profile/Profile.js
@@ -5,7 +5,7 @@ import ReactJson from "react-json-view";
 import useSWR from "swr";
 import {fetchWithUser} from "../../Util/fetch";
 import BreadcrumbBar from "../../Components/BreadcrumbBar/BreadcrumbBar";
-import "./Profile.css";
+import styles from "./Profile.module.css";
 const Profile = () => {
   const awsToken = process.env.REACT_APP_MANAGEMENT_API_KEY;
   const { user } = useAuth0();
@@ -16,20 +16,20 @@ const Profile = () => {
   return (
     <div>
       <BreadcrumbBar page="Profile" />
-      <div className="card">
-        <div className="card-body">
+      <div className={styles["card"]}>
+        <div className={styles["card-body"]}>
           <form>
             <div className="row form-row">
               <div className="col-12 col-md-12">
-                <div className="form-group">
-                  <div className="change-avatar">
-                    <div className="profile-img">
+                <div className={styles["form-group"]}>
+                  <div className={styles["change-avatar"]}>
+                    <div className={styles["profile-img"]}>
                       <img src={picture} alt="User" />
                     </div>
                     <div className="upload-img">
-                      <div className="change-photo-btn">
+                      <div className={styles["change-photo-btn"]}>
                           <span><i className="fa fa-upload"></i> Upload Photo</span>
-                          <input type="file" className="upload" />
+                          <input type="file" className={styles["upload"]} />
                       </div>
                       <small className="form-text text-muted">Allowed JPG, GIF or PNG. Max size of 2MB</small>
                     </div>
@@ -37,29 +37,29 @@ const Profile = () => {
                 </div>
               </div>
               <div className="col-12 col-md-6">
-                <div className="form-group">
+                <div className={styles["form-group"]}>
                   <label>First Name</label>
-                  <input type="text" className="form-control" value="Richard" />
+                  <input type="text" className={styles["form-control"]} defaultValue="Richard" />
                 </div>
               </div>
               <div className="col-12 col-md-6">
-                <div className="form-group">
+                <div className={styles["form-group"]}>
                   <label>Last Name</label>
-                  <input type="text" className="form-control" value="Wilson" />
+                  <input type="text" className={styles["form-control"]} defaultValue="Wilson" />
                 </div>
               </div>
               <div className="col-12 col-md-6">
-                <div className="form-group">
+                <div className={styles["form-group"]}>
                   <label>Date of Birth</label>
                   <div className="cal-icon">
-                    <input type="text" className="form-control datetimepicker" value="24-07-1983" />
+                    <input type="text" className={styles["form-control"] + " " + "datetimepicker"} defaultValue="24-07-1983" />
                   </div>
                 </div>
               </div>
               <div className="col-12 col-md-6">
-                <div className="form-group">
+                <div className={styles["form-group"] + " " + styles["select-wrapper"]}>
                   <label>Blood Group</label>
-                  <select className="form-control select">
+                  <select className={styles["form-control"]}>
                     <option>A-</option>
                     <option>A+</option>
                     <option>B-</option>
@@ -72,60 +72,60 @@ const Profile = () => {
                 </div>
               </div>
               <div className="col-12 col-md-6">
-                <div className="form-group">
+                <div className={styles["form-group"]}>
                   <label>Age</label>
-                  <input type="age" className="form-control" value="37"/>
+                  <input type="age" className={styles["form-control"]} defaultValue="37"/>
                 </div>
               </div>
               <div className="col-12 col-md-6">
-                <div className="form-group">
+                <div className={styles["form-group"] + " " + styles["select-wrapper"]}>
                   <label>Sex</label>
-                  <select className="form-control select">
+                  <select className={styles["form-control"] + " " + "select"}>
                     <option>Male</option>
                     <option>Female</option>
                   </select>
                 </div>
               </div>
               <div className="col-12 col-md-6">
-                <div className="form-group">
+                <div className={styles["form-group"]}>
                   <label>Email ID</label>
-                  <input type="email" className="form-control" value="richard@example.com"/>
+                  <input type="email" className={styles["form-control"]} defaultValue="richard@example.com"/>
                 </div>
               </div>
               <div className="col-12 col-md-6">
-                <div className="form-group">
+                <div className={styles["form-group"]}>
                   <label>Mobile</label>
-                  <input type="text" value="+1 202-555-0125" className="form-control"/>
+                  <input type="text" defaultValue="+1 202-555-0125" className={styles["form-control"]}/>
                 </div>
               </div>
               <div className="col-12">
-                <div className="form-group">
+                <div className={styles["form-group"]}>
                 <label>Address</label>
-                  <input type="text" className="form-control" value="806 Twin Willow Lane"/>
+                  <input type="text" className={styles["form-control"]} defaultValue="806 Twin Willow Lane"/>
                 </div>
               </div>
               <div className="col-12 col-md-6">
-                <div className="form-group">
+                <div className={styles["form-group"]}>
                   <label>City</label>
-                  <input type="text" className="form-control" value="Old Forge"/>
+                  <input type="text" className={styles["form-control"]} defaultValue="Old Forge"/>
                 </div>
               </div>
               <div className="col-12 col-md-6">
-                <div className="form-group">
+                <div className={styles["form-group"]}>
                   <label>State</label>
-                  <input type="text" className="form-control" value="Newyork"/>
+                  <input type="text" className={styles["form-control"]} defaultValue="Newyork"/>
                 </div>
               </div>
               <div className="col-12 col-md-6">
-                <div className="form-group">
+                <div className={styles["form-group"]}>
                   <label>Zip Code</label>
-                  <input type="text" className="form-control" value="13420"/>
+                  <input type="text" className={styles["form-control"]} defaultValue="13420"/>
                 </div>
               </div>
               <div className="col-12 col-md-6">
-                <div className="form-group">
+                <div className={styles["form-group"]}>
                   <label>Country</label>
-                  <input type="text" className="form-control" value="United States"/>
+                  <input type="text" className={styles["form-control"]} defaultValue="United States"/>
                 </div>
               </div>
             </div>
