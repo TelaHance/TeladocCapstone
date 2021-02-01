9b2192fe6264f35e1a5514d20b500156c45fce51
Alan Roddick <aroddick16@gmail.com>
2021-01-31 17:47:56 -0800

    Added ability to remove symptoms and added conditions

--- telehance-dashboard/src/main/Pages/ConsultDashboard/Consult/Diagnoses/Diagnoses.js
+++ telehance-dashboard/src/main/Pages/ConsultDashboard/Consult/Diagnoses/Diagnoses.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import classes from './Diagnoses.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faCommentAlt, faSearch, faStethoscope, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faCheck, faCommentAlt, faSearch, faStethoscope, faTimes } from '@fortawesome/free-solid-svg-icons';
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
+
+
 
 const Diagnoses = ({
   question,
@@ -10,33 +14,62 @@ const Diagnoses = ({
 }) => {
   console.log(symptoms)
   const [active, setActive] = useState(null);
+  const [symptomsState, setSymptoms] = useState(symptoms);
+  console.log(symptomsState)
+
+  function changeSymptom(symptomData) {
+    let updatedSymptomData = symptomData;
+    updatedSymptomData.choice_id = updatedSymptomData.choice_id == "present" ? "absent" : "present";
+    let tempSymptoms = symptomsState;
+    tempSymptoms[tempSymptoms.indexOf(symptomData)] = updatedSymptomData
+    setSymptoms([...tempSymptoms]);
+  }
+
   return (
     <>
-    <div className={classes.infermedica}>
+    <div className={active ? classes.infermedica + ' ' + classes.infermedicaActive : classes.infermedica}>
       {active == "questions" ? <div className={classes.question}>
         <h4>Question</h4>
         <div>{question}</div>
       </div> : null}
       {active == "conditions" ? <div className={classes.condition}>
-        <h4>Medical Conditions</h4>
-        {medicalConditions.map((medicalCondition) => (
-          <ul>
-            <li key={medicalCondition.id} className='list-group-item'>
-              <strong>Common Name</strong>: {medicalCondition.common_name}
-            </li>
-            <li key={medicalCondition.id} className='list-group-item'>
-              <strong>Name</strong>: {medicalCondition.name}
-            </li>
-            <li key={medicalCondition.id} className='list-group-item'>
-              <strong>Probability</strong>: {medicalCondition.probability}
-            </li>
-          </ul>
+      <h4>Diagnoses</h4>
+      <h5>Suggested Question</h5>
+      <div>{question}</div>
+        {medicalConditions.map((condition) => (
+          <Dropdown className={classes.item} key={condition.id}>
+            <Dropdown.Toggle variant="success" className={classes.dropdownToggle}>
+              <div>
+                {Math.round(condition.probability * 100) + '%'}
+              </div>
+              <div>
+                <div className={classes.name}>
+                  {condition.name}
+                </div>
+                <div className={classes.commonName}>
+                  {condition.common_name}
+                </div>
+              </div>
+              <button className={classes.expand}>
+                <FontAwesomeIcon 
+                  icon={faAngleDown}
+                />
+              </button>
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item>
+                <h1>Test</h1>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+            
+          </Dropdown>
+          
         ))}
       </div> : null}
       {active == "symptoms" ? <div className={classes.symptom}>
         <h4>Symptoms Analysis</h4>
-        {symptoms.map((symptomData) => (
-          <div key={symptomData.id} className={classes.item}>
+        {symptomsState.map((symptomData) => (
+          <div className={classes.item}>
             <FontAwesomeIcon 
               icon={symptomData.choice_id == "present" ? faCheck : faTimes} 
               style={{ color: symptomData.choice_id == "present" ? "green" : "red"}}
@@ -50,6 +83,14 @@ const Diagnoses = ({
                 {symptomData.common_name}
               </div>
             </div>
+            <button className={classes.update} 
+            title={symptomData.choice_id == "present" ? "Remove Symptom" : "Add Symptom"}
+            onClick={() => changeSymptom(symptomData)}>
+              <FontAwesomeIcon 
+                icon={symptomData.choice_id == "present" ? faTimes : faCheck} 
+                style={{ color: symptomData.choice_id == "present" ? "red" : "green"}}
+              />
+            </button>
           </div>
         ))}
       </div> : null}
