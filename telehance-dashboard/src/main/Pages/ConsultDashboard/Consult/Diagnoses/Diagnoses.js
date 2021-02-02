f1712225b1191a9c7ecfec8e8120b086c981cfb5
Alan Roddick <aroddick16@gmail.com>
2021-02-01 16:57:02 -0800

    Updated transitions and cleaned up code

--- telehance-dashboard/src/main/Pages/ConsultDashboard/Consult/Diagnoses/Diagnoses.js
+++ telehance-dashboard/src/main/Pages/ConsultDashboard/Consult/Diagnoses/Diagnoses.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import classes from './Diagnoses.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown, faArrowLeft, faArrowRight, faCheck, faCommentAlt, faMinusCircle, faPlusCircle, faSearch, faStethoscope, faTimes, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
-import { ProgressBar } from 'react-bootstrap';
+import { faAngleDown, faAngleUp, faArrowLeft, faArrowRight, faCheck, faCommentAlt, faMinusCircle, faPlusCircle, faSearch, faStethoscope, faTimes, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { Accordion, AccordionContext, Card, ProgressBar } from 'react-bootstrap';
 
 
 
@@ -11,11 +11,10 @@ const Diagnoses = ({
   medicalConditions,
   symptoms
 }) => {
-  console.log(symptoms)
   const [active, setActive] = useState(null);
   const [symptomsState, setSymptoms] = useState(symptoms);
-  const [expanded, setExpanded] = useState(false);
-  console.log(symptomsState)
+  const [expandedSide, setExpandedSide] = useState(false);
+  const [expandedContent, setExpandedContent] = useState(false);
 
   function changeSymptom(symptomData) {
     let updatedSymptomData = symptomData;
@@ -25,67 +24,76 @@ const Diagnoses = ({
     setSymptoms([...tempSymptoms]);
   }
 
-  function changeVisibility(id) {
-    // document.getElementById(id).style.display = document.getElementById(id).style.display === "none" ? "" : "none";
-    document.getElementById(id).classList.contains(classes.inactive) ? 
-      document.getElementById(id).classList.remove(classes.inactive) :
-      document.getElementById(id).classList.add(classes.inactive);
-  }
-
   function toggleExpanded() {
-    if (expanded || active) {
-      setExpanded(false);
-      setActive(null);
+    if (expandedSide || expandedContent) {
+      setExpandedSide(false);
+      setExpandedContent(false);
     }
     else {
-      setExpanded(true);
+      setExpandedSide(true);
     }
   }
 
-  return (
-    <>
-    <div className={active ? classes.infermedica + ' ' + classes.infermedicaActive : classes.infermedica}>
-      {active === "questions" ? <div className={classes.question}>
-        <h4>Question</h4>
-        <div>{question}</div>
-      </div> : null}
-      {active === "conditions" ? <div className={classes.condition}>
+  function ContextAwareArrow( { children, eventKey, callback }) {
+    const currentEventKey = useContext(AccordionContext);
+
+    const isCurrentEventKey = currentEventKey === eventKey;
+
+    return (
+      <FontAwesomeIcon
+        icon={isCurrentEventKey ? faAngleUp : faAngleDown}
+        style={{"margin-left": "auto", "width": "1em"}}
+      >
+        {children}
+      </FontAwesomeIcon>
+    );
+  }
+
+  function renderConditions() {
+    return (
+      <div className={classes.condition}>
       <h4>Diagnoses</h4>
       <h5>Suggested Question</h5>
       <div style={{"margin-bottom": "1rem"}}>{question}</div>
+      <Accordion className={classes.itemContainer}>
         {medicalConditions.map((condition) => (
-          <>
-          <button className={classes.item} key={condition.id} onClick={() => changeVisibility(condition.id)}>
-            <div>
-              <ProgressBar 
-                now={Math.round(condition.probability * 100)} 
-                variant={Math.round(condition.probability * 100) > 66 ? "success" : 
-                  (Math.round(condition.probability * 100) > 33 ? "warning" : "danger")}
-                className={classes.progressBar}
-              />
-              {Math.round(condition.probability * 100) + '%'}
-            </div>
-            <div>
-              <div className={classes.name}>
-                {condition.name}
+          <Card className={classes.card}>
+            <Accordion.Toggle as={Card.Header} eventKey={condition.id} className={classes.item + ' ' + classes['card-header']}>
+              <div>
+                <ProgressBar 
+                  now={Math.round(condition.probability * 100)} 
+                  variant={Math.round(condition.probability * 100) > 66 ? "success" : 
+                    (Math.round(condition.probability * 100) > 33 ? "warning" : "danger")}
+                  className={classes.progressBar}
+                />
+                {Math.round(condition.probability * 100) + '%'}
               </div>
-              <div className={classes.commonName}>
-                {condition.common_name}
+              <div>
+                <div className={classes.name}>
+                  {condition.name}
+                </div>
+                <div className={classes.commonName}>
+                  {condition.common_name}
+                </div>
               </div>
-            </div>
-            <FontAwesomeIcon 
-              icon={faAngleDown}
-              className={classes.expand}
-            />
-          </button>
-          <div id={condition.id} className={classes.item + ' ' + classes.inactive}>
-            <h1>Test</h1>
-          </div>
-          </>
+              <ContextAwareArrow eventKey={condition.id} />
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey={condition.id}>
+              <Card.Body className={classes.item}>
+                Test
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
           
         ))}
-      </div> : null}
-      {active === "symptoms" ? <div className={classes.symptom}>
+      </Accordion>
+      </div>
+    );
+  }
+
+  function renderSymptoms() {
+    return(
+      <div className={classes.symptom}>
         <h4>Symptoms Analysis</h4>
         <div className={classes.search}>
           <input placeholder="Search"></input>
@@ -94,11 +102,6 @@ const Diagnoses = ({
               style={{ color: "green"}}
               className={classes.icon}
           />
-          {/* <FontAwesomeIcon 
-            icon={faMinusCircle} 
-            style={{ color: "red"}}
-            className={classes.icon}
-          /> */}
         </div>
         <div className={classes.itemContainer}>
           {symptomsState.map((symptomData) => (
@@ -130,31 +133,51 @@ const Diagnoses = ({
           ))}
         </div>
         
-      </div> : null}
-    </div>
-    <div className={classes.sidebar}>
-      <button onClick={() => {setActive("symptoms"); setExpanded(false)}} className={active === "symptoms" ? classes.active : null}>
-        <FontAwesomeIcon icon={faSearch}/>
-        {expanded && <div>
-          Symptoms
-        </div>}
-      </button>
-      <button onClick={() => {setActive("conditions"); setExpanded(false)}} className={active === "conditions" ? classes.active : null}>
-        <FontAwesomeIcon icon={faStethoscope}/>
-        {expanded && <div>
-          Diagnoses
-        </div>}
-      </button>
-      <button onClick={() => {setActive("questions"); setExpanded(false)}} className={active === "questions" ? classes.active : null}>
-        <FontAwesomeIcon icon={faCommentAlt}/>
-        {expanded && <div>
-          Doctor's Notes
-        </div>}
-      </button>
-      <button onClick={() => toggleExpanded()} style={{"margin-top": "auto"}}>
-        <FontAwesomeIcon icon={expanded || active ? faArrowRight : faArrowLeft}/>
-      </button>
-    </div>
+      </div>
+    )
+  }
+
+  function renderSidebar() {
+    return (
+      <div className={classes.sidebar}>
+        <button onClick={() => {setActive("symptoms"); setExpandedSide(false); setExpandedContent(true)}} 
+          className={expandedContent && active === "symptoms" ? classes.active : null}>
+          <FontAwesomeIcon icon={faSearch}/>
+          {expandedSide && <div>
+            Symptoms
+          </div>}
+        </button>
+        <button onClick={() => {setActive("conditions"); setExpandedSide(false); setExpandedContent(true)}} 
+          className={expandedContent && active === "conditions" ? classes.active : null}>
+          <FontAwesomeIcon icon={faStethoscope}/>
+          {expandedSide && <div>
+            Diagnoses
+          </div>}
+        </button>
+        <button onClick={() => {setActive("notes"); setExpandedSide(false); setExpandedContent(true)}} 
+          className={expandedContent && active === "notes" ? classes.active : null}>
+          <FontAwesomeIcon icon={faCommentAlt}/>
+          {expandedSide && <div>
+            Doctor's Notes
+          </div>}
+        </button>
+        <button onClick={() => toggleExpanded()} style={{"margin-top": "auto"}}>
+          <FontAwesomeIcon icon={expandedSide || expandedContent ? faArrowRight : faArrowLeft}/>
+        </button>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div className={expandedContent ? classes.infermedica + ' ' + classes.infermedicaActive : classes.infermedica}>
+        {active === "notes" ? <div className={classes.question}>
+          <h4>Doctor's Notes</h4>
+        </div> : null}
+        {active === "conditions" ? renderConditions() : null}
+        {active === "symptoms" ? renderSymptoms() : null}
+      </div>
+      {renderSidebar()}
     </>
   )
 }
