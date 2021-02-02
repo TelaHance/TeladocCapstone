32c268683ece5ba52121cf38c0fdb52af0968172
GrantClark209 <grantclark1999@gmail.com>
2021-02-02 14:42:32 -0800

    Formatter

--- telehance-dashboard/src/main/Pages/Patients/TwilioCall.js
+++ telehance-dashboard/src/main/Pages/Patients/TwilioCall.js
@@ -1,69 +1,83 @@
-import React, {useEffect} from "react";
-import { Image, Button, Col, Row} from "react-bootstrap";
-import Container from "react-bootstrap/Container";
+import React, { useEffect } from 'react';
+import { Image, Button, Col, Row } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 import { Device } from 'twilio-client';
-import fetch from "isomorphic-unfetch";
-import {useAuth0} from "@auth0/auth0-react";
-import phoneIcon from "../../assets/phoneIcon.png";
-import { useHistory } from "react-router-dom";
+import fetch from 'isomorphic-unfetch';
+import { useAuth0 } from '@auth0/auth0-react';
+import phoneIcon from '../../assets/phoneIcon.png';
+import { useHistory } from 'react-router-dom';
 
-const TwilioCall = ({match}) => {
-    const { user } = useAuth0();
-    const { sub } = user;
-    const { params:{phoneNumber, patientId} } = match;
-    const history = useHistory();
-    useEffect(() => {
-        fetch('https://59wncxd6oi.execute-api.us-west-2.amazonaws.com/dev/get-token')
-            .then( r => r.json())
-            .then(data => {
-                Device.setup(JSON.parse(data).token,{closeProtection: true});
-            }).catch(err => console.log(err));
-        });
-    function call() {
-        const d = new Date();
+const TwilioCall = ({ match }) => {
+  const { user } = useAuth0();
+  const { sub } = user;
+  const {
+    params: { phoneNumber, patientId },
+  } = match;
+  const history = useHistory();
+  useEffect(() => {
+    fetch(
+      'https://59wncxd6oi.execute-api.us-west-2.amazonaws.com/dev/get-token'
+    )
+      .then((r) => r.json())
+      .then((data) => {
+        Device.setup(JSON.parse(data).token, { closeProtection: true });
+      })
+      .catch((err) => console.log(err));
+  });
+  function call() {
+    const d = new Date();
 
-        const params = {
-            "callTo": phoneNumber,
-            "doctor_id": sub.split('|')[1],
-            "patient_id": patientId,
-            "timestamp":d.getTime()
-        };
-        Device.connect(params);
-    }
+    const params = {
+      callTo: phoneNumber,
+      doctor_id: sub.split('|')[1],
+      patient_id: patientId,
+      timestamp: d.getTime(),
+    };
+    Device.connect(params);
+  }
 
-    function hangup() {
-        Device.disconnectAll();
-        history.push('/consults')
-    }
+  function hangup() {
+    Device.disconnectAll();
+    history.push('/consults');
+  }
 
-    return (
-        <>
-            <h1>Patient Consultation</h1>
-            <Container className="mb-5">
-                <Row>
-                    <Col>
-                        <Image
-                            width={200}
-                            height={200}
-                            src={phoneIcon}
-                            alt="Profile"
-                            rounded
-                            fluid
-                        />
-                    </Col>
-                </Row>
-                <br/>
-                <Row>
-                    <Col md={{ span: 3, offset: 3 }}>
-                        <Button className="call" onClick={call} size="lg" variant="success">Call</Button>
-                    </Col>
-                    <Col md={{ span: 3, offset: 0 }}>
-                         <Button className="hangup" onClick={hangup} size="lg" variant="danger">Hangup</Button>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <h1>Patient Consultation</h1>
+      <Container className='mb-5'>
+        <Row>
+          <Col>
+            <Image
+              width={200}
+              height={200}
+              src={phoneIcon}
+              alt='Profile'
+              rounded
+              fluid
+            />
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col md={{ span: 3, offset: 3 }}>
+            <Button className='call' onClick={call} size='lg' variant='success'>
+              Call
+            </Button>
+          </Col>
+          <Col md={{ span: 3, offset: 0 }}>
+            <Button
+              className='hangup'
+              onClick={hangup}
+              size='lg'
+              variant='danger'
+            >
+              Hangup
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default TwilioCall;
