e48e6cc12bce07e04b7046107a4b0585c23f26c2
GrantClark209 <grantclark1999@gmail.com>
2021-02-18 20:02:35 -0800

    WIP

--- telehance-dashboard/src/main/App.js
+++ telehance-dashboard/src/main/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Route, Switch } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import Profile from './Pages/Profile/Profile';
+import Home from 'Pages/Home/Home';
+import Profile from 'Pages/Profile/Profile';
 import Appointment from 'Pages/Appointment';
 import AppointmentDashboard from 'Pages/AppointmentDashboard';
-import ConsultDashboard from './Pages/ConsultDashboard';
-import Consult from './Pages/Consult';
+import ConsultDashboard from 'Pages/ConsultDashboard';
+import Consult from 'Pages/Consult';
 import Spinner from 'Components/Spinner';
-import Admin from './Pages/Admin/Admin';
-import AuthorizedRoute from './Components/Auth/AuthorizedRoute';
-import PrivateRoute from './Components/Auth/PrivateRoute';
-import Layout from './Components/Layout';
+import Admin from 'Pages/Admin/Admin';
+import AuthorizedRoute from 'Components/Auth/AuthorizedRoute';
+import PrivateRoute from 'Components/Auth/PrivateRoute';
+import Layout from 'Components/Layout';
 import classes from './App.module.css';
 
 function App() {
--- telehance-dashboard/src/main/Components/Layout/index.tsx
+++ telehance-dashboard/src/main/Components/Layout/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import Navbar from '../Nav/Navbar';
+import { Application } from 'react-rainbow-components';
+import Navbar from 'Components/Nav/Navbar';
 import classes from './Layout.module.css';
+
+// Default, but we can edit these later to provide colors to the application.
+const theme = {
+  rainbow: {
+    palette: {
+      brand: '#01B6F5',
+      success: '#1DE9B6',
+      error: '#FE4849',
+      warning: '#FC0',
+      mainBackground: '#FFF',
+    },
+  },
+};
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className={classes.container}>
+    <Application theme={theme} className={classes.container}>
       <Navbar />
       <div className={classes.content}>{children}</div>
-    </div>
+    </Application>
   );
 }
 
