18b4d58997a00f9aa279c93590a5e5144a0eac7d
GrantClark209 <grantclark1999@gmail.com>
2021-02-22 17:49:10 -0800

    Theme + trivial changes

--- telehance-dashboard/src/main/Components/Layout/index.tsx
+++ telehance-dashboard/src/main/Components/Layout/index.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import { Application } from 'react-rainbow-components';
 import Navbar from 'Components/Nav/Navbar';
 import classes from './Layout.module.css';
 
-// Default, but we can edit these later to provide colors to the application.
-const theme = {
-  rainbow: {
-    palette: {
-      brand: '#01B6F5',
-      success: '#1DE9B6',
-      error: '#FE4849',
-      warning: '#FC0',
-      mainBackground: '#FFF',
-    },
-  },
-};
-
 export default function Layout({ children }: LayoutProps) {
   return (
-    <Application theme={theme} className={classes.container}>
+    <div className={classes.container}>
       <Navbar />
       <div className={classes.content}>{children}</div>
-    </Application>
+    </div>
   );
 }
 
