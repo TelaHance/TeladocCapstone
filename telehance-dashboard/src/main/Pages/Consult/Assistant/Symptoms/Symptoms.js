84188b324ebd717d5359c2d38b037fcba7bb8f7a
Alan Roddick <aroddick16@gmail.com>
2021-02-28 14:48:47 -0800

    Switching assistant format to separate doctor-added conditions from predicted conditions

--- telehance-dashboard/src/main/Pages/Consult/Assistant/Symptoms/Symptoms.js
+++ telehance-dashboard/src/main/Pages/Consult/Assistant/Symptoms/Symptoms.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { putWithToken } from 'Util/fetch';
 import symptomsJson from 'assets/symptoms';
+import conditionsJson from 'assets/conditions';
 import classes from '../Assistant.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faMinusCircle, faPlusCircle, faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -13,7 +14,8 @@ const Symptoms = ({
   isLive
 }) => {
   const awsToken = process.env.REACT_APP_CONSULT_API_KEY;
-  const [symptomsState, setSymptoms] = useState(symptoms ? [...symptoms] : null);
+  const [symptomsState, setSymptoms] = useState(symptoms ? [...symptoms] : []);
+  const [conditionsState, setConditions] = useState([]);
   const [searchState, setSearchState] = useState({ options: null });
 
   useEffect(() => {
@@ -34,7 +36,7 @@ const Symptoms = ({
     width: '100%',
   };
 
-  const symptomDatabase =
+  const database =
     symptomsJson.map(symptom => {
       return ({
         id: symptom.id,
@@ -44,11 +46,25 @@ const Symptoms = ({
       })
     })
 
+  database.push(...
+    conditionsJson.map(condition => {
+      return ({
+        id: condition.id,
+        label: condition.name,
+        description: condition.common_name,
+      })
+    })
+  )
+  console.log(database)
+
   function filter(query, options) {
     if (query) {
       return options.filter(item => {
         const regex = new RegExp(query, 'i');
-        return regex.test(item.label) || regex.test(item.description) && symptomsState.every(symptom => symptom.id !== item.id);
+        if (regex.test(item.label) || regex.test(item.description)) {
+          return conditionsState.every(condition => condition.id !== item.id) &&
+          symptomsState.every(symptom => symptom.id !== item.id);
+        }
       });
     }
     return [];
@@ -68,7 +84,7 @@ const Symptoms = ({
       setTimeout(
         () =>
           setSearchState({
-            options: filter(value, symptomDatabase),
+            options: filter(value, database),
             isLoading: false,
           }),
         500,
@@ -100,18 +116,40 @@ const Symptoms = ({
     saveSymptoms(tempSymptoms);
   }
 
-  function addSymptom() {
-    let tempSymptoms = symptomsState ? [...symptomsState] : [];
-    const newSymptom = {
-      choice_id: "present",
-      common_name: searchState.option.description,
-      id: searchState.option.id,
-      name: searchState.option.label,
-      type: "symptom"
+  function removeCondition(conditionData) {
+    if(!conditionsState) {
+      return;
     }
-    tempSymptoms.push(newSymptom);
-    setSymptoms(tempSymptoms);
-    saveSymptoms(tempSymptoms);
+    let tempConditions = conditionsState.filter(condition => condition.id !== conditionData.id);
+    setConditions(tempConditions);
+  }
+
+  function addEntity() {
+    if (searchState.option.id[0] === 's') {
+      let tempSymptoms = symptomsState ? [...symptomsState] : [];
+      const newSymptom = {
+        choice_id: "present",
+        common_name: searchState.option.description,
+        id: searchState.option.id,
+        name: searchState.option.label,
+        type: "symptom"
+      }
+      tempSymptoms.push(newSymptom);
+      setSymptoms(tempSymptoms);
+      saveSymptoms(tempSymptoms);
+    } else if (searchState.option.id[0] === 'c') {
+      let tempConditions = conditionsState ? [...conditionsState] : [];
+      const newCondition = {
+        common_name: searchState.option.description,
+        id: searchState.option.id,
+        name: searchState.option.label,
+        type: "condition"
+      }
+      tempConditions.push(newCondition);
+      console.log(tempConditions);
+      setConditions(tempConditions);
+      // saveConditions(tempConditions);
+    }
   }
 
   function saveSymptoms(tempSymptoms) {
@@ -121,11 +159,11 @@ const Symptoms = ({
 
   return (
     <div className={classes.content}>
-      <h4>Symptoms Analysis</h4>
+      <h4>Analysis</h4>
       <div className={classes.search}>
         <Lookup
           id="lookup-3"
-          placeholder="Add Symptoms"
+          placeholder="Add"
           size="medium"
           options={searchState.options}
           value={searchState.option}
@@ -136,9 +174,9 @@ const Symptoms = ({
           style={containerStyles}
           className={classes.lookup}
         />
-        <button title="Add Symptom"
+        <button title="Add"
           onClick={() => {
-            addSymptom();
+            addEntity();
             setSearchState({ option: null })
           }}>
           <FontAwesomeIcon
@@ -148,7 +186,9 @@ const Symptoms = ({
           />
         </button>
       </div>
-      {symptomsState && <div className={classes.itemContainer}>
+      {symptomsState && symptomsState.length > 0 && <><h5>Symptoms</h5>
+      <div className={classes.itemContainer}>
+        
         {symptomsState.map((symptomData) => (
           <div className={classes.item} key={symptomData.id}>
             <button title="Toggle Present"
@@ -176,7 +216,30 @@ const Symptoms = ({
             </button>
           </div>
         ))}
-      </div>}
+      </div></>}
+      {conditionsState && conditionsState.length > 0 && <><h5>Conditions</h5>
+      <div className={classes.itemContainer}>
+        
+        {conditionsState.map((conditionData) => (
+          <div className={classes.item} key={conditionData.id}>
+            <div>
+              <div className={classes.name}>
+                {conditionData.name}
+              </div>
+              <div className={classes.commonName}>
+                {conditionData.common_name}
+              </div>
+            </div>
+            <button className={classes.update} onClick={() => removeCondition(conditionData)}
+              title="Remove Condition">
+              <FontAwesomeIcon
+                icon={faMinusCircle}
+                style={{ color: "red" }}
+              />
+            </button>
+          </div>
+        ))}
+      </div></>}
     </div>
   )
 }
