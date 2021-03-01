c0b31c5930076e787e3e78d160b86f5056f5af91
GrantClark209 <grantclark1999@gmail.com>
2021-03-01 11:37:01 -0800

    React spring before impl.

--- telehance-dashboard/src/main/Components/Assistant/Conditions/Conditions.js
+++ telehance-dashboard/src/main/Components/Assistant/Conditions/Conditions.js
@@ -1,43 +1,43 @@
-import React from "react";
+import React from 'react';
 import classes from '../Assistant.module.css';
 import { ProgressBar } from 'react-bootstrap';
 
-const Conditions = ({
-  medicalConditions,
-  question
-}) => {
-
+const Conditions = ({ medicalConditions, question }) => {
   return (
     <div className={classes.content}>
       <h4>Intelligent Diagnostic Assistant</h4>
       <h5>Suggested Question</h5>
-      {question && <div style={{ "marginBottom": "1rem" }}>{question}</div>}
-      {medicalConditions && <div className={classes.itemContainer}>
-        {medicalConditions.map((condition) => (
-          <div className={classes.item} key={condition.id}>
-            <div>
-              <ProgressBar
-                now={Math.round(condition.probability * 100)}
-                variant={Math.round(condition.probability * 100) > 66 ? "success" :
-                  (Math.round(condition.probability * 100) > 33 ? "warning" : "danger")}
-                className={classes.progressBar}
-              />
-              {Math.round(condition.probability * 100) + '%'}
-            </div>
-            <div>
-              <div className={classes.name}>
-                {condition.name}
+      {question && <div style={{ marginBottom: '1rem' }}>{question}</div>}
+      {medicalConditions && (
+        <div className={classes.itemContainer}>
+          {medicalConditions.map((condition) => (
+            <div className={classes.item} key={condition.id}>
+              <div>
+                <ProgressBar
+                  now={Math.round(condition.probability * 100)}
+                  variant={
+                    Math.round(condition.probability * 100) > 66
+                      ? 'success'
+                      : Math.round(condition.probability * 100) > 33
+                      ? 'warning'
+                      : 'danger'
+                  }
+                  className={classes.progressBar}
+                />
+                {Math.round(condition.probability * 100) + '%'}
               </div>
-              <div className={classes.commonName}>
-                {condition.common_name}
+              <div>
+                <div className={classes.name}>{condition.name}</div>
+                <div className={classes.commonName}>
+                  {condition.common_name}
+                </div>
               </div>
             </div>
-          </div>
-
-        ))}
-      </div>}
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Conditions;
