c0af3efe30ed0a77a9b0feeb49228e93af08ce2a
Lu Han <luhan@ucsb.edu>
2021-03-01 14:32:24 -0500

    rainbow consult dashboard

--- telehance-dashboard/src/main/Pages/ConsultDashboard/getColumns.tsx
+++ telehance-dashboard/src/main/Pages/ConsultDashboard/getColumns.tsx
@@ -1,151 +1,180 @@
 import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, useHistory } from 'react-router-dom';
 import { ColumnDescription } from 'react-bootstrap-table-next';
-import { textFilter } from 'react-bootstrap-table2-filter';
-import Button from 'react-bootstrap/Button';
 import { ConsultData, UserData } from 'Models';
 import Sentiment from '../../Components/Sentiment';
 import SentimentFilter, { filter } from '../../Components/Sentiment/Filter';
 import classes from './ConsultDashboard.module.css';
+import {Button} from "react-rainbow-components";
 
 // FORMATTERS
 
-const dateFormatter = (cell: number, row: ConsultData) => {
-  if (typeof cell === 'string') cell = parseInt(cell);
-  return new Date(cell).toLocaleString('default', {
-    month: 'long',
-    day: '2-digit',
-    year: 'numeric',
+export function dateFormatter (value:any) {
+  if (typeof value.value === 'string') value.value = parseInt(value.value);
+  const start = new Date(value.value);
+  const date = start.toLocaleString('default', {
+      month: 'long',
+      day: '2-digit',
+      year: 'numeric',
   });
-};
+  const time = start.toLocaleString('default', {
+      hour: '2-digit',
+      minute:'2-digit',
+      hour12: true,
+  });
+  return(
+      <div className={classes['date']}>
+          <span> {date} </span>
+          <br/>
+          <span> {time} </span>
+      </div>
+  );
+}
 
-const csvDateFormatter = (cell: number | string, row: ConsultData) => {
-  if (typeof cell === 'string') cell = parseInt(cell);
-  return new Date(cell).toISOString().split('T')[0]; // yyyy-mm-dd
-};
+// const dateFormatter = (cell: number, row: ConsultData) => {
+//   if (typeof cell === 'string') cell = parseInt(cell);
+//   return new Date(cell).toLocaleString('default', {
+//     month: 'long',
+//     day: '2-digit',
+//     year: 'numeric',
+//   });
+// };
 
-const nameFormatter = (cell: UserData, row: ConsultData) => {
+// const csvDateFormatter = (cell: number | string, row: ConsultData) => {
+//   if (typeof cell === 'string') cell = parseInt(cell);
+//   return new Date(cell).toISOString().split('T')[0]; // yyyy-mm-dd
+// };
+
+// const firstNameFormatter = (cell: UserData, row: ConsultData) => {
+//   return cell.given_name;
+// };
+
+// const lastNameFormatter = (cell: UserData, row: ConsultData) => {
+//   return cell.family_name;
+// };
+
+export function nameFormatter(cell:any){
   return(
     <div>
-      <img className={classes['rounded-circle']} src={cell.picture} width="31" alt=""></img>
-      <span> {cell.given_name} {cell.family_name} </span>
+      <img className="rounded-circle" src={cell.value.picture} width="31" alt=""></img>
+      <span> {cell.value.given_name} {cell.value.family_name} </span>
     </div>
   )
-};
+}
 
-const sentimentFormatter = (cell?: any, row?: ConsultData) => {
-  if (typeof cell === 'number') return `${Math.round(cell * 100)}%`;
-  return <Sentiment sentiment={cell} />;
-};
+export function sentimentFormatter(cell:any){
+  if (cell.value === 'number') return (<div> <span>`${Math.round(cell.value * 100)}%`</span></div>);
+  return <Sentiment sentiment={cell.value} />;
+}
 
-const csvSentimentFormatter = (cell?: number, row?: ConsultData) => {
-  if (cell) return cell;
-  return -1;
-};
+// const csvSentimentFormatter = (cell?: number, row?: ConsultData) => {
+//   if (cell) return cell;
+//   return -1;
+// };
 
-const buttonFormatter = (
-  cell: any,
-  row: ConsultData,
-  history: RouteComponentProps['history']
-) => {
+export function ButtonFormatter(
+  cell: any
+){
+  const history = useHistory()
   return (
-    <Button onClick={() => history.push(`/consults/${row.consult_id}`)}>
-      View
-    </Button>
+    <div className={classes['view-container']}>
+      <Button onClick={() => history.push(`/consults/${cell.value}`)}>
+        View
+      </Button>
+    </div>
   );
-};
+}
 
 // COLUMN DESCRIPTIONS
 
-const start_date = {
-  dataField: 'start_time',
-  text: 'Appointment Date',
-  formatter: dateFormatter,
-  csvFormatter: csvDateFormatter,
-  sort: true,
-};
+// const start_date = {
+//   dataField: 'start_time',
+//   text: 'Consult Date',
+//   formatter: dateFormatter,
+//   csvFormatter: csvDateFormatter,
+//   sort: true,
+// };
 
-const doctor = [
-  {
-    dataField: 'doctor.given_name',
-    text: 'Doctor First Name',
-    hidden: true,
-  },
-  {
-    dataField: 'doctor.family_name',
-    text: 'Doctor Last Name',
-    hidden: true,
-  },
-  {
-    dataField: 'doctor',
-    text: 'Doctor',
-    formatter: nameFormatter,
-    csvExport: false,
-  },
-];
+// const doctor: ColumnDescription[] = [
+//   {
+//     dataField: 'doctor.given_name',
+//     text: 'Doctor First Name',
+//     hidden: true,
+//   },
+//   {
+//     dataField: 'doctor.family_name',
+//     text: 'Doctor Last Name',
+//     hidden: true,
+//   },
+//   {
+//     dataField: 'doctor',
+//     text: 'Doctor',
+//     formatter: nameFormatter,
+//     csvExport: false,
+//   },
+// ];
 
-const patient: ColumnDescription[] = [
-  {
-    dataField: 'patient.given_name',
-    text: 'Patient First Name',
-    hidden: true,
-  },
-  {
-    dataField: 'patient.family_name',
-    text: 'Patient Last Name',
-    hidden: true,
-  },
-  {
-    dataField: 'patient',
-    text: 'Patient',
-    formatter: nameFormatter,
-    csvExport: false,
-  },
-];
+// const patient: ColumnDescription[] = [
+//   {
+//     dataField: 'patient.given_name',
+//     text: 'Patient First Name',
+//     hidden: true,
+//   },
+//   {
+//     dataField: 'patient.family_name',
+//     text: 'Patient Last Name',
+//     hidden: true,
+//   },
+//   {
+//     dataField: 'patient',
+//     text: 'Patient',
+//     formatter: nameFormatter,
+//     csvExport: false,
+//   },
+// ];
 
-const sentiment = {
-  dataField: 'sentiment',
-  text: 'Issues',
-  formatter: sentimentFormatter,
-  csvFormatter: csvSentimentFormatter,
-  csvType: Number,
-  headerClasses: classes.issues,
-  filter: textFilter({
-    // @ts-ignore
-    onFilter: filter
-  }),
-  filterRenderer: (onFilter: any, column: any) => (
-    <SentimentFilter onFilter={onFilter} column={column} />
-  ),
-};
+// const sentiment = {
+//   dataField: 'sentiment',
+//   text: 'Issues',
+//   formatter: sentimentFormatter,
+//   csvType: Number,
+//   headerClasses: classes.issues,
+//   // filter: textFilter({
+//   //   // @ts-ignore
+//   //   onFilter: filter
+//   // }),
+//   filterRenderer: (onFilter: any, column: any) => (
+//     <SentimentFilter onFilter={onFilter} column={column} />
+//   ),
+// };
 
-function viewConsult(history: RouteComponentProps['history']) {
-  return {
-    dataField: 'button',
-    text: 'Actions',
-    formatter: (cell: any, row: any) => buttonFormatter(cell, row, history),
-    csvExport: false,
-  };
-}
+// export function viewConsult(history: RouteComponentProps['history']) {
+//   return {
+//     field: 'button',
+//     header: 'Actions',
+//     formatter: (cell: any, row: any) => buttonFormatter(cell, row, history),
+//     csvExport: false,
+//   };
+// }
 
 // PRIMARY FUNCTION
 
-export default function getColumns(
-  history: RouteComponentProps['history'],
-  role: string
-): ColumnDescription[] {
+// export default function getColumns(
+//   history: RouteComponentProps['history'],
+//   role: string
+// ): ColumnDescription[] {
 
-  let columns: ColumnDescription[] = [start_date];
-  if (role !== 'DOCTOR') {
-    columns = columns.concat(doctor);
-  }
-  if (role !== 'PATIENT') {
-    columns = columns.concat(patient);
-  }
-  if (role === 'ADMIN') {
-    columns = columns.concat(sentiment);
-  }
-  columns.push(viewConsult(history));
+//   let columns: ColumnDescription[] = [start_date];
+//   if (role !== 'DOCTOR') {
+//     columns = columns.concat(doctor);
+//   }
+//   if (role !== 'PATIENT') {
+//     columns = columns.concat(patient);
+//   }
+//   if (role === 'ADMIN') {
+//     columns = columns.concat(sentiment);
+//   }
+//   columns.push(viewConsult(history));
 
-  return columns;
-}
+//   return columns;
+// }
