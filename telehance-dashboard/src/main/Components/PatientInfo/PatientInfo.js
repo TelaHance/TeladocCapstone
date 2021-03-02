c9e34f84fd3dc3e6a703448794348d610edcbdf0
GrantClark209 <grantclark1999@gmail.com>
2021-03-02 15:21:31 -0800

    Slight modifications to PatientInfo

--- telehance-dashboard/src/main/Components/PatientInfo/PatientInfo.js
+++ telehance-dashboard/src/main/Components/PatientInfo/PatientInfo.js
@@ -1,32 +1,21 @@
-import React from "react";
-import classes from '../../../Components/Assistant/Assistant.module.css';
+import React from 'react';
+import classes from './PatientInfo.module.css';
 
-const PatientInfo = ({
-  name,
-  picture,
-  purpose
-}) => {
-
+export default function PatientInfo({ name, picture, purpose }) {
   return (
-    <div>
+    <div className={classes.container}>
       <div className={classes.patient}>
-        <img src={picture} className="rounded-circle" width={70} />
-        <h6>{name}</h6>
-        {purpose ? <div>
+        <img src={picture} className='rounded-circle' width={90} />
+        <div>
+          <h5>{name}</h5>
+        </div>
+      </div>
+      {purpose && (
+        <div className={classes.purpose}>
           <h7>Reason For Visit</h7>
-          <div className={classes.itemContainer}>
-            <div className={classes.item}>
-              {purpose}
-            </div>
-          </div>
-        </div> : null}
-      </div>
-
-      <div>
-
-      </div>
+          <div>{purpose}</div>
+        </div>
+      )}
     </div>
   );
 }
-
-export default PatientInfo;
