602b5bb313da31e117c0ff057ee1251729c6f4cb
GrantClark209 <grantclark1999@gmail.com>
2021-03-02 15:25:56 -0800

    Added patientinfo to appointments

--- telehance-dashboard/src/main/Components/PatientInfo/PatientInfo.js
+++ telehance-dashboard/src/main/Components/PatientInfo/PatientInfo.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './PatientInfo.module.css';
 
-export default function PatientInfo({ name, picture, purpose }) {
+export default function PatientInfo({ patient, purpose }) {
+  const { given_name, family_name, picture } = patient;
   return (
     <div className={classes.container}>
       <div className={classes.patient}>
         <img src={picture} className='rounded-circle' width={90} />
         <div>
-          <h5>{name}</h5>
+          <h5>{given_name + ' ' + family_name}</h5>
         </div>
       </div>
       {purpose && (
